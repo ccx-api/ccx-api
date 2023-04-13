43efc6c46883d843a5a97deeb5db97e906f9348c
delef <delef@ya.ru>
2023-04-13 08:54:55 +0400

    [Coinbase] Fixed ProductTicker fields type

--- rust/ccx/api/coinbase/src/api/exchange/product/types/product_ticker.rs
+++ rust/ccx/api/coinbase/src/api/exchange/product/types/product_ticker.rs
@@ -2,18 +2,18 @@ use crate::api::exchange::prelude::*;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub struct ProductTicker {
+    /// The best ask price in the quote currency.
+    pub ask: Decimal,
+    /// The best bid price in the quote currency.
+    pub bid: Decimal,
+    /// The total trading volume for the last 24 hours in the base currency.
+    pub volume: Decimal,
     /// The unique identifier for the trade.
     pub trade_id: i64,
     /// The price of the trade in the quote currency.
-    pub price: String,
+    pub price: Decimal,
     /// The size of the trade in the base currency.
-    pub size: String,
+    pub size: Decimal,
     /// The time of the trade.
     pub time: String,
-    /// The best bid price in the quote currency.
-    pub bid: String,
-    /// The best ask price in the quote currency.
-    pub ask: String,
-    /// The total trading volume for the last 24 hours in the base currency.
-    pub volume: String,
 }
