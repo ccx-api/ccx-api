5b65cc8ef93dd318134907334e405df7cde6d0fe
alexander-irbis <irbis.labs@gmail.com>
2023-12-18 10:39:51 +0200

    [Gate] Update the wallet example.

--- rust/ccx/api/gate/examples/wallet.rs
+++ rust/ccx/api/gate/examples/wallet.rs
@@ -1,6 +1,7 @@
 use ccx_api_lib::GateApiCred;
 use ccx_gate::client::rest::RequestError;
 use ccx_gate::GateApi;
+// use rust_decimal_macros::dec;
 
 #[actix_rt::main]
 async fn main() -> Result<(), RequestError> {
@@ -10,7 +11,8 @@ async fn main() -> Result<(), RequestError> {
 
     let api = GateApi::<GateApiCred>::from_env();
 
-    // dbg!(api.wallet_balances(None).await)?;
+    dbg!(api.wallet_balances(None).await)?;
+    // Currently is not working. The only way to transfer funds is to use the website.
     // dbg!(
     //     api.wallet_transfer(
     //         "USDT".into(),
@@ -26,6 +28,17 @@ async fn main() -> Result<(), RequestError> {
         api.wallet_withdrawal_history(None, None, None, None, None)
             .await
     )?;
+    // dbg!(
+    //     api.withdrawal_withdraw(
+    //         Some("client id".into()),
+    //         dec!(2.63),
+    //         "USDT".into(),
+    //         Some("Txxx ... wallet address here ...".into()),
+    //         None,
+    //         "TRX".into(),
+    //     )
+    //     .await
+    // )?;
 
     Ok(())
 }
