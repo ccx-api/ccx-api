a6b39925b56c0d81b1f7d1bec7021120afa0e8d2
ArsileLuci <42055415+ArsileLuci@users.noreply.github.com>
2024-02-13 21:08:42 +0300

    [binance-pay] Add API method for adding sub-merchants (#22)
    * [binance-pay] Add API method for adding sub merchants
    * [binance-pay] Minor corrections

--- rust/ccx/api/binance-pay/src/api/mod.rs
+++ rust/ccx/api/binance-pay/src/api/mod.rs
@@ -16,6 +16,7 @@ mod create_order_v2;
 mod query_order_v1;
 mod query_order_v2;
 mod response;
+mod submerchant_add_v1;
 mod transfer_fund_v1;
 mod transfer_fund_v2;
 mod webhook;
@@ -35,6 +36,8 @@ pub mod prelude {
     pub use super::query_order_v2::V2QueryOrderRequest;
     pub use super::query_order_v2::V2QueryOrderResult;
     pub use super::response::BinancePayResponse;
+    pub use super::submerchant_add_v1::V1SubmerchantAddRequest;
+    pub use super::submerchant_add_v1::V1SubmerchantAddResult;
     pub use super::transfer_fund_v1::V1TransferFundRequest;
     pub use super::transfer_fund_v1::V1TransferResult;
     pub use super::transfer_fund_v2::V2TransferFundRequest;
