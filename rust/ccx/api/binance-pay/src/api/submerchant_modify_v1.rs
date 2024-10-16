2759c60dd471f47b3f03df1d2ab6493d99e58a8b
ArsileLuci <42055415+ArsileLuci@users.noreply.github.com>
2024-10-16 20:01:51 +0300

    [BinancePay] Add blockPayerKycCountries to SubMerchant methods (#27)

--- rust/ccx/api/binance-pay/src/api/submerchant_modify_v1.rs
+++ rust/ccx/api/binance-pay/src/api/submerchant_modify_v1.rs
@@ -139,6 +139,12 @@ pub struct V1SubmerchantModifyRequest {
     /// Contract date with ISV
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub contract_time_isv: Option<i64>,
+
+    /// ISO Alpha 2 country codes.
+    /// Blocks payers from specified countries based on their KYC country.
+    /// The list contains the countries to be blocked.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub block_payer_kyc_countries: Option<Vec<String>>,
 }
 
 /// Response data placeholder
@@ -187,7 +193,8 @@ mod tests {
             "certificateCountry":"US",
             "certificateNumber":"123456X",
             "certificateValidDate":1752422400000,
-            "contractTimeIsv":1594656000000
+            "contractTimeIsv":1594656000000,
+            "blockPayerKycCountries": ["US", "AE"]
         }
         "#;
         let request: V1SubmerchantModifyRequest =
@@ -220,6 +227,7 @@ mod tests {
             certificate_number: Some("123456X".to_owned()),
             certificate_valid_date: Some(1752422400000),
             contract_time_isv: Some(1594656000000),
+            block_payer_kyc_countries: Some(vec!["US".to_owned(), "AE".to_owned()]),
         };
         let json = serde_json::to_string(&request).expect("Failed to_string");
         println!("test_serde_add_submerchant_individual_request :: {}", json);
@@ -251,7 +259,8 @@ mod tests {
             "certificateCountry":null,
             "certificateNumber":null,
             "certificateValidDate":null,
-            "contractTimeIsv":1594656000000
+            "contractTimeIsv":1594656000000,
+            "blockPayerKycCountries": ["US", "AE"]
         }
         "#;
         let request: V1SubmerchantModifyRequest =
@@ -284,6 +293,7 @@ mod tests {
             certificate_number: None,
             certificate_valid_date: None,
             contract_time_isv: Some(1594656000000),
+            block_payer_kyc_countries: Some(vec!["US".to_owned(), "AE".to_owned()]),
         };
         let json = serde_json::to_string(&request).expect("Failed to_string");
         println!(
