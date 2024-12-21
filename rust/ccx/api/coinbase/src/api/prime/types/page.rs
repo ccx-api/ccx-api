84883af78738da3e04f3991dc1a2d88f30c51538
alexander-irbis <irbis.labs@gmail.com>
2024-12-21 21:17:48 +0200

    Extend pagination options.

--- rust/ccx/api/coinbase/src/api/prime/types/page.rs
+++ rust/ccx/api/coinbase/src/api/prime/types/page.rs
@@ -7,7 +7,7 @@ pub struct Page {
     #[serde(default, skip_serializing_if = "Option::is_none")]
     limit: Option<u32>,
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    sort_direction: Option<String>,
+    sort_direction: Option<SortDirection>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
@@ -17,14 +17,34 @@ pub struct NextPage {
     #[serde(default, skip_serializing_if = "Option::is_none")]
     limit: Option<u32>,
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    sort_direction: Option<String>,
+    sort_direction: Option<SortDirection>,
     has_next: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy, Eq, PartialEq)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum SortDirection {
+    Asc,
+    Desc,
 }
 
 impl Page {
     pub fn with_limit(mut self, limit: u32) -> Self {
         self.limit = Some(limit);
         self
+    }
+
+    pub fn with_sort_direction(mut self, sort_direction: SortDirection) -> Self {
+        self.sort_direction = Some(sort_direction);
+        self
+    }
+
+    pub fn asc(mut self) -> Self {
+        self.with_sort_direction(SortDirection::Asc)
+    }
+
+    pub fn desc(mut self) -> Self {
+        self.with_sort_direction(SortDirection::Desc)
     }
 
     pub fn cursor(&self) -> Option<&str> {
@@ -35,8 +55,8 @@ impl Page {
         self.limit.as_ref()
     }
 
-    pub fn sort_direction(&self) -> Option<&str> {
-        self.sort_direction.as_deref()
+    pub fn sort_direction(&self) -> Option<SortDirection> {
+        self.sort_direction
     }
 }
 
