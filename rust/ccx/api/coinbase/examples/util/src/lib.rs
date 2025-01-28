d9f862d82a876f6eb5f3b1409ed2a55424a50c37
alexander-irbis <irbis.labs@gmail.com>
2025-01-28 22:45:10 +0200

    Add prepared custom env-logger formatters to examples' utils.

--- rust/ccx/api/coinbase/examples/util/src/lib.rs
+++ rust/ccx/api/coinbase/examples/util/src/lib.rs
@@ -14,3 +14,35 @@ pub fn print_res<T: fmt::Debug>(res: CoinbaseApiResult<T>) -> CoinbaseApiResult<
 pub fn d(v: &'static str) -> Decimal {
     v.parse().unwrap()
 }
+
+pub fn log_format_clean(
+    buf: &mut env_logger::fmt::Formatter,
+    record: &log::Record<'_>,
+) -> std::io::Result<()> {
+    use std::io::Write;
+
+    writeln!(
+        buf,
+        "{} [{}] - {}",
+        chrono::Utc::now().format("%Y-%m-%dT%H:%M:%S"),
+        record.level(),
+        record.args(),
+    )
+}
+
+pub fn log_format_full(
+    buf: &mut env_logger::fmt::Formatter,
+    record: &log::Record<'_>,
+) -> std::io::Result<()> {
+    use std::io::Write;
+
+    writeln!(
+        buf,
+        "{} [{}] - {}, {}:{}",
+        chrono::Utc::now().format("%Y-%m-%dT%H:%M:%S"),
+        record.level(),
+        record.args(),
+        record.file().unwrap_or("unknown"),
+        record.line().unwrap_or(0),
+    )
+}
